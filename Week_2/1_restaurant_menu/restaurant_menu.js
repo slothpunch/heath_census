b074ebb809fb7dc5477c9aa217b78e0113d2df3c
slothpunch <giminsub1@gmail.com>
2024-05-14 17:14:23 +1000

    map, foeEach, for

---
+++ Week_2/1_restaurant_menu/restaurant_menu.js
@@ -0,0 +1,25 @@
+const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+// With .map(). As index + 1, index stars from 1
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+// With .forEach(). As index + 1, index stars from 1
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+    mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;
+});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+// With for loop
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;
+};
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
+
+
+
+
---
+++ prac_file.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+// W-1-3. product_survey.js // 
+
+// W-1-4. product_survey.js // 
+
+// W-1-5. product_survey.js // 
+
+
+// W-2-1. restaurant_menu.js // 
+const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+// With .map()
+breakfastMenuItemsHTML
+
+// With .forEach()
+mainCourseItem
+
+// With for loop
+dessertItem
+
+
+
