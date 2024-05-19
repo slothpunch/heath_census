a81c3b9de2a9db7bec1b3d20c1a5da555f9c8f1c
slothpunch <giminsub1@gmail.com>
2024-05-19 22:19:36 +1000

    todo_list

---
+++ Week_3/2_todo_list/todo_list.js
@@ -0,0 +1,16 @@
+const taskInput = document.getElementById('taskInput');
+
+const addTaskBtn = document.getElementById('addTaskBtn');
+const taskList = document.getElementById('taskList');
+const clearCompletedBtn = document.getElementById('clearCompletedBtn');
+
+let tasks = [];
+
+function addTask() {
+    const taskText = taskInput.textContent;   
+}
+
+
+
+
+
