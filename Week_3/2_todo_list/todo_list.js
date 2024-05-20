7230589d8feb8958439428966794238e4cfa09c5
slothpunch <giminsub1@gmail.com>
2024-05-20 22:09:20 +1000

    todo_list

--- Week_3/2_todo_list/todo_list.js
+++ Week_3/2_todo_list/todo_list.js
@@ -7,10 +7,41 @@ const clearCompletedBtn = document.getElementById('clearCompletedBtn');
 let tasks = [];
 
 function addTask() {
-    const taskText = taskInput.textContent;   
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== "") {
+        tasks.push({text: taskText});
+        taskInput.value = ""; 
+        displayTasks();
+    }
 }
 
+function displayTasks() {
+    taskList.innerHTML = "";
+    tasks.forEach((task, idx) => {
+        const li = document.createElement("li");
+        // a ternary operator 
+        li.innerHTML=`<input type="checkbox" id="task-${idx}" ${task.completed ? "checked" : ""}>
+        <label for="task-${idx}">${task.text}</label>`;
+        li.querySelector("input").addEventListener("change", () => toggleTask(idx));
+        taskList.appendChild(li);
+    });
 
+}
 
+function toggleTask(idx) {
+    // It helps by selecting the checkbox regardless. 
+    // If selected, then it will mark that particular task as completed.
+    tasks[idx].completed = !tasks[idx].completed;
+    displayTasks();
+}
 
+function clearCompletedTasks() {
+    tasks = tasks.filter(task => !task.completed);
+    // tasks = tasks.filter((task) => {!task.completed}); -> not working
+    displayTasks();
+}
 
+addTaskBtn.addEventListener("click", addTask);
+clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+displayTasks();
