8101a1445dc509aca44b22073e8398923bde58d6
slothpunch <giminsub1@gmail.com>
2024-06-03 21:57:55 +1000

    weather_API

--- Week_4/fetch_data.js
+++ Week_4/fetch_data.js
@@ -1,6 +1,52 @@
+fetch(url, options)
+    .then(response => {
+        // Handle the response
+    })
+    .catch(error => {
+        // Handle any errors that occurred during the fetch
+    });
+
+fetch(url, {
+    method: 'GET' // POST, PUT, DELETE, PATCH, OPTIONS
+})
+
+fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({ name: 'John', email: 'email'}),
+    header: {
+        'Content-Type': 'application/json'
+    }
+})
+
+fetch(url, {
+    method: 'PUT',
+    body: JSON.stringify({name: 'Updated_name', email: 'updated@example.com'}),
+    header: {
+        'Content-Type': 'application/json'
+    }
+})
+
+fetch(url, {
+    method: 'DELETE'
+})
+
+// Partial modification
+fetch(url, {
+    method: 'PATCH',
+    body: JSON.stringify({name: 'anotherUpdateName'}),
+    header: {
+        'Content-Type': 'application/json'
+    }
+})
+
+// Describe communcation options; check allowed methods and headers
+fetch(url, {
+    method: 'OPTIONS'
+})
+
 // Function to fetch and work with JSON data
 function fetchBooks() {
-	// Fetch the JSON data from the file
+	// Fetch the JSON data from the file. GET is the default method?
 	fetch('books.json')
         .then(response => response.json())
         .then(data => {
@@ -19,3 +65,5 @@ function fetchBooks() {
             console.error('Error fetching JSON data:', error);
         });
 }
+
+
