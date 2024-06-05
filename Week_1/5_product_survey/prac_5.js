bf24bfb6c5551235ef2b36697cd2b862f91b068f
INSUB KIM <42757351+slothpunch@users.noreply.github.com>
2024-06-05 16:14:35 +1000

    Delete Week_1 directory

--- Week_1/5_product_survey/prac_5.js
+++
@@ -1,8 +0,0 @@
-
-// 1. Create a function: submitFeedback
-// 1-1. Fetch username, age, email, job, designation, product type and feedback from HTML and save into variables 
-// 1-2. Display the information in the div where display is none.
-// 3. When the submit button is clicked, submitFeedback is executed. Also, when the Enter key is pressed, submitFeedback is executed. 
-
-
-
